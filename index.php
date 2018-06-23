ca70227e99f44492ad3f6817aafab1cae84b2bd3
Asiro <takahiroasiro@gmail.com>
2018-06-23 15:33:43 +0600

    Creating html structure for register page

--- index.php
+++ index.php
@@ -1,20 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <title>Bootstrap</title>
+</head>
+<body>
+<div class="container mt-4">
+    <form action="index.html" method="post">
+        <fieldset class="form-group">
+            <legend>Register</legend>
+            <div class="form-group form-row">
+                <div class="form-group col-auto">
+                    <label class="form-control-label">
+                        Enter your login:
+                        <input class="form-control" type="text" name="login">
+                    </label>
+                </div> <!-- form-group -->
+                <div class="form-group col-auto">
+                    <label class="form-control-label">
+                        Enter your email:
+                        <input class="form-control" type="email" name="login">
+                    </label>
+                    <small class="form-text text-muted">We'll never share your email.</small>
+                </div> <!-- form-group -->
+            </div> <!-- form-group -->
+            <div class="form-group form-row">
+                <div class="form-group col-auto">
+                    <label class="form-control-label">
+                        Enter your password:
+                        <input class="form-control" type="password" name="password">
+                    </label>
+                </div> <!-- form-group -->
+                <div class="form-group col-auto">
+                    <label class="form-control-label">
+                        Confirm your password:
+                        <input class="form-control" type="password" name="confirm-password">
+                    </label>
+                </div> <!-- form-group -->
+            </div> <!-- form-group -->
+            <button class="btn btn-primary" type="submit">Register</button>
+        </fieldset> <!-- fieldset -->
+    </form>
+</div><!-- container -->
 
-    </head>
-    <body>
-        <div class="form-group">
-            <input type="text" name="login" placeholder="Enter your login" class="form-control">
-            <input type="password" name="password" placeholder="Enter your password">
-            <input type="password" name="confirm-password" placeholder="Confirm your password">
-        </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    </body>
-
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
